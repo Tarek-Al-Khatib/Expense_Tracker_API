9d8538d2c34bec5f0ca4ecd65a24c925967368aa
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-08 19:50:39 +0200

    Added user id to add and get methods

--- transactions/add.php
+++ transactions/add.php
@@ -7,9 +7,10 @@ $type = $_POST['type'];
 $amount = $_POST['amount'];
 $date = $_POST['date'];
 $notes = $_POST['notes'];
+$user_id = $_POST['userid'];
 
-$query = $connection->prepare("INSERT INTO transactions (type, amount, date, notes) VALUES (?, ?, ?, ?)");
-$query->bind_param("sdss", $type, $amount, $date, $notes);
+$query = $connection->prepare("INSERT INTO transactions (type, amount, date, notes, user_id) VALUES (?, ?, ?, ?,?)");
+$query->bind_param("sdssi", $type, $amount, $date, $notes, $user_id);
 $query->execute();
 
 echo json_encode(["status" => "success"]);
--- transactions/get.php
+++ transactions/get.php
@@ -2,8 +2,9 @@
 include "../connection.php";
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
+$id = $_POST['id'] ?? null;
 
-$query = $connection->prepare("SELECT * FROM transactions");
+$query = $connection->prepare("SELECT * FROM transactions where user_id = $id");
 $query->execute();
 $transactions = [];
 $result = $query->get_result();
